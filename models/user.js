219495c45f5595272c52371692dea55cd106b1b4
Jeevan Babu Satyam <116184803+jeevanleo@users.noreply.github.com>
2024-01-21 19:48:15 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type: String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type: String,
+        required : true
+    }
+},{
+    timestamps : true
+})
+
+const User = mongoose.model('User',userSchema);
+module.exports = User;
+
+// const mongoose = require('mongoose');
+
+// const userSchema = new mongoose.Schema({
+//     email:{
+//         type: String,
+//         required:true,
+//         unique:true
+//     },
+//     password:{
+//         type: String,
+//         required : true
+//     },
+//     name:{
+//         type:String,
+//         required : true
+//     }
+// },{
+//     timestamps : true
+// })
+
+// const User = mongoose.model('User',userSchema);
+// module.exports = User;
